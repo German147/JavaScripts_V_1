b91eae0b1d2ac2d2e3b9c8a8e6f242a6d7773dd9
German <barreragerman27@gmail.com>
2022-09-23 17:42:11 -0300

    It was created the example of arrays on JS.

---
+++ ArraysIn.js
@@ -0,0 +1,16 @@
+//Arrays examples
+
+var firstName = 'Alan';
+
+var names = ['Alan',
+    'John',
+    'Judy',
+    'German',
+    'MoreNames'];
+
+console.log(names[0])
+console.log(names[1])
+console.log(names[2])
+console.log(names[3])
+console.log(names[4])
+console.log('The size of this array is ' + names.length)
